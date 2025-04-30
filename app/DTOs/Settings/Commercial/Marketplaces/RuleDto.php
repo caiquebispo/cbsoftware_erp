b956d9dbd79ff017d92f5b1bbbe2471f3c17f815
Caique Bispo <diogo@guimepa.com.br>
2025-04-30 15:03:11 -0300

    wip

--- app/DTOs/Settings/Commercial/Marketplaces/RuleDto.php
+++ app/DTOs/Settings/Commercial/Marketplaces/RuleDto.php
@@ -17,8 +17,8 @@ final readonly class RuleDto
     {
         return [
             'GMP014_GMP015_Id' => $this->canal,
-            'GMP014_Comissao' => $this->comissao,
-            'GMP014_Custo_Sac' => $this->custoSac,
+            'GMP014_Comissao' => $this->comissao ?? 0,
+            'GMP014_Custo_Sac' => $this->custoSac ?? 0,
             'GMP014_Comissao_Sobre_Frete' => $this->comissaoFrete ? 'S' : 'N',
             'GMP014_Multiplicar_Pela_Quantidade' => $this->multiplicarQt ? 'S' : 'N',
         ];
