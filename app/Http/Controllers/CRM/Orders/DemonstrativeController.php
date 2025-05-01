310b2ca2c82ef62823efd37aba1da79551f6d211
Caique Bispo <caiquebispo0023@gmail.com>
2025-05-01 15:21:37 -0700

    feat: criando o controlador do demonstrativo de margem de ipv

--- app/Http/Controllers/CRM/Orders/DemonstrativeController.php
+++ app/Http/Controllers/CRM/Orders/DemonstrativeController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\CRM\Orders;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
 
 class DemonstrativeController extends Controller
 {
-    //
+    public function index(): View
+    {
+        return view('panel.crm.orders.demonstrative.index');
+    }
 }
