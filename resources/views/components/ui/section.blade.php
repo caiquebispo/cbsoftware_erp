1cb5d8eba7543fe319f4d90528d60441a48dd374
Caique Bispo <caiquebispo0023@gmail.com>
2025-05-01 15:46:08 -0700

    refactor: criando efeito sonfona no componente de section

--- resources/views/components/ui/section.blade.php
+++ resources/views/components/ui/section.blade.php
@@ -1,40 +1,51 @@
 @props([
-'title' => null,
-'actions' => null,
-'padding' => 'p-6',
-'border' => false,
-'shadow' => true,
-'rounded' => 'rounded-lg',
-'background' => 'bg-white',
-'titleClass' => '',
-'containerClass' => ''
+    'padding' => 'p-6',
+    'border' => false,
+    'shadow' => true,
+    'rounded' => 'rounded-lg',
+    'background' => 'bg-white',
+    'containerClass' => '',
+    'collapsed' => false
 ])
 
-<div {{ $attributes->merge(['class' => implode(' ', [
-    $background,
-    $padding,
-    $rounded,
-    $border ? 'border border-gray-200' : '',
-    $shadow ? 'shadow' : '',
-    $containerClass
-    ])]) }}>
-    @if($title || $actions)
-    <div class="flex justify-between items-center mb-6">
-        @if($title)
-        <h2 class="{{ $titleClass }} text-lg font-bold text-gray-800">
-            {{ $title }}
-        </h2>
-        @endif
-
-        @if($actions)
-        <div class="flex items-center space-x-2">
-            {{ $actions }}
+<div
+    x-data="{ open: {{ $collapsed ? 'false' : 'true' }} }"
+    {{ $attributes->merge(['class' => implode(' ', [
+        $background,
+        $padding,
+        $rounded,
+        $border ? 'border border-gray-200' : '',
+        $shadow ? 'shadow' : '',
+        $containerClass
+    ])]) }}
+>
+    {{-- Slot para o header --}}
+    <div class="flex justify-between items-start mb-4">
+        <div class="flex-1">
+            {{ $header ?? '' }}
         </div>
-        @endif
-    </div>
-    @endif
 
-    <div class="{{ $slot->isEmpty() ? 'text-gray-500' : '' }}">
+        {{-- Toggle button (caso tenha conteúdo no body) --}}
+        <button
+            @click="open = !open"
+            class="ml-4 text-gray-500 hover:text-gray-800 transition"
+            title="Exibir/ocultar conteúdo"
+        >
+            <svg x-show="open" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none"
+                 viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M5 15l7-7 7 7" />
+            </svg>
+            <svg x-show="!open" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none"
+                 viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M19 9l-7 7-7-7" />
+            </svg>
+        </button>
+    </div>
+
+    {{-- Slot do conteúdo (body) --}}
+    <div x-show="open" x-transition x-collapse>
         {{ $slot }}
     </div>
 </div>
